fe432c9d9639aeb1b2c25d47cb67e01d0d8a2b19
Juanap12 <juanpablocj@gmail.com>
2018-11-05 23:49:47 -0500

    Cambios con Sprites

---
+++ Assets/Scripts/HiglightObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiglightObject : MonoBehaviour {
+
+    private Color startColor;
+    public Color mouseOverColor;
+    bool mouseOver = false;
+
+	private void OnMouseEnter()
+	{
+        startColor = GetComponent<Renderer>().material.color;
+        mouseOver = true;
+        GetComponent<Renderer>().material.SetColor("_Color", mouseOverColor);
+	}
+
+	private void OnMouseExit()
+	{
+        mouseOver = false;
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+	}
+}
