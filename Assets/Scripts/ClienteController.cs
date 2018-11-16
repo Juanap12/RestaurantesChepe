1aa749612cfa0955d50d288522d857d106730b74
Juanap12 <juanpablocj@gmail.com>
2018-11-16 00:10:34 -0500

    Creación Cliente

---
+++ Assets/Scripts/ClienteController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClienteController : MonoBehaviour {
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public Animator animatorChepe;
+
+    public SpriteRenderer spriteChepe;
+
+    // fila, carta, comiendo, ...
+    private string estado;
+
+    private bool selected;
+
+    // Use this for initialization
+    void Start()
+    {
+        estado = "fila";
+        selected = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (selected == true)
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    // Move our Agent
+                    agent.SetDestination(hit.point);
+
+                    //animatorChepe.SetBool("ChepeIsMoving", true);
+
+                    if (hit.point.x > this.transform.position.x)
+                    {
+                        spriteChepe.flipX = false;
+                    }
+                    else
+                    {
+                        spriteChepe.flipX = true;
+                    }
+
+                    //Debug.Log(Input.mousePosition.x);
+                    Debug.Log(hit.point.x);
+                }
+            }
+            
+        }
+        
+
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    // Done
+                    //animatorChepe.SetBool("ChepeIsMoving", false);
+                    selected = false;
+                }
+            }
+        }
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (estado.Equals("fila"))
+        {
+            selected = true;
+        }
+    }
+}
--- Assets/Scripts/HiglightObject.cs
+++ Assets/Scripts/HiglightObject.cs
@@ -23,4 +23,5 @@ public class HiglightObject : MonoBehaviour {
         mouseOver = false;
         spriteObjeto.color = startColor;
 	}
+
 }
